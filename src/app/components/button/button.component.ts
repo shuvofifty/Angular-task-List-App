bae0ebb59db2c3dc6e52c6a43254002bf74a80e8
Brototi Biswas <72146916+brototibiswas@users.noreply.github.com>
2023-09-02 20:37:25 -0400

    header component with button

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+})
+export class ButtonComponent implements OnInit {
+  @Input() text: string = '';
+  @Input() color: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
